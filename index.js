0ca2b8bb5405414cdf258a0a720e7739cb850adf
ibrahimsifat <ibsifat24@gmail.com>
2024-10-23 17:11:21 +0300

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+//setup full express server
+const express = require("express");
+const app = express();
+const port = 3000;
+const { readFile } = require("fs");
+
+app.get("/", (req, res) => {
+  readFile("./index.html", "utf-8", (err, html) => {
+    if (err) {
+      res.status(500).send("Service Unavailable");
+    }
+    res.send(html);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
