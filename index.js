0d8940ab8ad9162c5bf30f14649ae7af736ecd28
ibrahimsifat <ibsifat24@gmail.com>
2024-10-23 17:30:06 +0300

    added:docker

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //setup full express server
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const { readFile } = require("fs");
 
 app.get("/", (req, res) => {
